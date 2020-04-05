4cdbcd977597238b87338c5b07a0e40c7f1b9c1e
Marco Canevaro <marcocanevaro@hotmail.com>
2020-04-05 17:39:58 -0300

    Market controller con index y create, cambios en rutas

---
+++ app/Http/Controllers/MarketController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Market;
+
+class MarketController extends Controller
+{
+    public function create(Request $request){
+        
+        $market = new Market;
+        $market->name = $request->name;
+        $market->save();
+
+        return $market;
+    
+    }
+
+    public function index(Request $request){
+        
+        $markets =  Market::all();
+
+        return $markets;
+    
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+Route::post('markets','MarketController@create')->name('createMarkets');
+
+Route::get('markets','MarketController@index')->name('indexMarkets');
