291ff935f6de364eea28250d96a34df7e8edd7c5
Marco Canevaro <marcocanevaro@hotmail.com>
2020-04-05 14:59:24 -0300

    Tablas markets, servers, categories y turns creadas

---
+++ database/migrations/2020_04_05_174822_create_turns_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTurnsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('turns', function (Blueprint $table) {
+            $table->bigIncrements('id');
+           
+            $table->string('code');
+            $table->unsignedBigInteger('server_id');
+            $table->foreign('server_id')->references('id')->on('servers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('turns');
+    }
+}
